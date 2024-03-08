df0b5348ff8a55d7c4d718d3a6ade9aa3e9040f9
PC <PC@DESKTOP-KGH00LN>
2024-03-08 17:30:20 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstAppHEAD
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int area = 2;
+            int height = 2;
+            int width = 2;
+
+            while (area<50)
+            {
+                height++;
+                area = width * height;
+
+            }
+            do
+            {
+                width--;
+                area = width * height;
+            } while (area>25);
+
+        }
+
+    }
+}
+
